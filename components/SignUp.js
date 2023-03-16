d8d714aa6ed1020ed62a344aa5bb8b644fc6fbb7
Chrsteffen <Chrsteffen1@gmail.com>
2023-03-15 20:19:46 -0400

    updated using Formik

--- components/SignUp.js
+++ components/SignUp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import InputField from './InputField';
 import { useState } from 'react';
+import {Formik} from 'formik'
 
 const SignUp = () => {
   const [email, setEmail]= useState('')
@@ -12,62 +13,78 @@ const SignUp = () => {
   const [breed, setBreed]= useState('')
   const [toy, setToy]= useState('')
 
-  function confirmPasswordMatch(props) {
-    const {nativeEvent: {text}} = props
-    if( text !== password) {
-      alert("Passwords must match")
+  function onSubmit(values) {
+    if(values.password !==values.confirmPassword){
+      alert("Passwords do not match")
     }
   }
 
 
+
+
   return (  
-    <View style={{justifyContent: 'center', backgroundColor: '#ecf0f1'}}>
-      <InputField 
-      label= 'email'
-      placeHolder= "Enter your email"
-      value={email}
-      onChangeText={setEmail}
-      />
-      <InputField 
-      label= 'password'
-      placeHolder= "Make a password"
-      value={password}
-      onChangeText={setPassword}
-      secureTextEntry={true}
-      />
-      <InputField 
-      label= 'Confirm password'
-      placeHolder= "Confirm your password"
-      value={confirmPassword}
-      onChangeText={setConfirmPassword}
-      secureTextEntry={true}
-      onSubmitEditing={confirmPasswordMatch}
-      />
-      <InputField 
-      label= 'Pets Name'
-      placeHolder= "Tell us your furry friends name"
-      value={petName}
-      onChangeText={setPetName}
-      />
-      <InputField 
-      label= 'pets date of birth'
-      placeHolder= "Enter their dob"
-      value={dob}
-      onChangeText={setDob}
-      />
-      <InputField 
-      label= 'breed'
-      placeHolder= "What breed are they"
-      value={breed}
-      onChangeText={setBreed}
-      />
-      <InputField 
-      label= 'favorite toy'
-      placeHolder= "What is their favorite toy"
-      value={toy}
-      onChangeText={setToy}
-      />
-    </View>
+    <Formik
+    initialValues={{
+      email: '',
+      password: '',
+      confirmPassword: '',
+      name: '',
+      dob: '',
+      breed: '',
+      toy: '',
+    }}
+    onSubmit={onSubmit}
+    >  
+    {({handleChange, handleSubmit, values})=>
+      <View style={{justifyContent: 'center', backgroundColor: '#ecf0f1'}}>
+        <InputField 
+        label= 'email'
+        placeHolder= "Enter your email"
+        value={values.email}
+        onChangeText={handleChange('email')}
+        />
+        <InputField 
+        label= 'password'
+        placeHolder= "Make a password"
+        value={values.password}
+        onChangeText={handleChange('password')}
+        secureTextEntry={true}
+        />
+        <InputField 
+        label= 'Confirm password'
+        placeHolder= "Confirm your password"
+        value={values.confirmPassword}
+        onChangeText={handleChange('confirmPassword')}
+        secureTextEntry={true}
+        onSubmitEditing={handleSubmit}
+        />
+        <InputField 
+        label= 'Pets Name'
+        placeHolder= "Tell us your furry friends name"
+        value={values.petName}
+        onChangeText={handleChange(petName)}
+        />
+        <InputField 
+        label= 'pets date of birth'
+        placeHolder= "Enter their dob"
+        value={values.dob}
+        onChangeText={handleChange('dob')}
+        />
+        <InputField 
+        label= 'breed'
+        placeHolder= "What breed are they"
+        value={values.breed}
+        onChangeText={handleChange('breed')}
+        />
+        <InputField 
+        label= 'favorite toy'
+        placeHolder= "What is their favorite toy"
+        value={values.toy}
+        onChangeText={handleChange('toy')}
+        />
+      </View>
+    }
+    </Formik>
   );
 }
 
